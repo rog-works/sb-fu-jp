e7f4eadbd911137caf7e85ebac14b63c1887359b
rog-works <rog_works@yahoo.co.jp>
2020-05-31 21:27:14 +0900

    feat: impl logging

---
+++ logger.py
@@ -0,0 +1,8 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
