7174518b3e32465baadfd7ca9daab5597304e917
rog-works <rog_works@yahoo.co.jp>
2020-05-31 23:05:09 +0900

    feat: reset comment out

--- 2-item-trans.sh
+++ 2-item-trans.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# for dir in `ls -F fu_assets/items | grep /`; do
-# 	files=`find fu_assets/items/${dir} -type f | egrep '(chest|legs|head|item|augment|currency|consumable|instrument)$' | awk -F, '{printf "%s,", $1}'`
-# 	time python app.py --dest dest --keys 'description' --files "${files}"
-# done
-
-files=`find  fu_assets/items/active -type f | egrep '(chest|legs|head|item|augment|currency|consumable|instrument)$' | awk -F, '{printf "%s,", $1}'`
-time python app.py --dest dest --keys 'description' --files "${files}"
+for dir in `ls -F fu_assets/items | grep /`; do
+	files=`find fu_assets/items/${dir} -type f | egrep '(chest|legs|head|item|augment|currency|consumable|instrument)$' | awk -F, '{printf "%s,", $1}'`
+	time python app.py --dest dest --keys 'description' --files "${files}"
+done
