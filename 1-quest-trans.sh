d8ce027225b465cf00a9fed83ba283a3fd1c9671
rog-works <rog_works@yahoo.co.jp>
2020-06-02 23:05:04 +0900

    fix: fix file args

--- 1-quest-trans.sh
+++ 1-quest-trans.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-files=`find fu_assets/quests/ -type f -name '*.questtemplate'`
+files=`find fu_assets/quests/ -type f -name '*.questtemplate' | awk -F, '{printf "%s,", $1}'`
 
-time python app.py --dest dest --keys 'text,title,completionText' ${files}
-
+time python app.py --dest dest --keys 'text,title,completionText' --files "${files}"
