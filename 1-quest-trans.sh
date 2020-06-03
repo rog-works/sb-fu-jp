31fad6c2b264bbdcb8a2e6ad81f83e0a0c9901b7
rog-works <rog_works@yahoo.co.jp>
2020-06-03 22:56:15 +0900

    fix: fix exec script

--- 1-quest-trans.sh
+++ 1-quest-trans.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-files=`find fu_assets/quests/ -type f -name '*.questtemplate' | awk -F, '{printf "%s,", $1}'`
+for dir in `ls -F fu_assets/quests | grep /`; do
+	files=`find fu_assets/quests/${dir} -type f -name '*.questtemplate' | awk -F, '{printf "%s,", $1}'`
+	python app.py --dest dest --keys 'text,title' --files "${files}"
+done
 
-time python app.py --dest dest --keys 'text,title' --files "${files}"
+files=`ls fu_assets/quests/*.questtemplate | awk -F, '{printf "%s,", $1}'`
+python app.py --dest dest --keys 'text,title' --files "${files}"
--- 2-item-trans.sh
+++ 2-item-trans.sh
@@ -2,5 +2,5 @@
 
 for dir in `ls -F fu_assets/items | grep /`; do
 	files=`find fu_assets/items/${dir} -type f | egrep '(activeitem|augment|back|beamaxe|chest|consumable|currency|flashlight|harvestingtool|head|instrument|item|legs|liqitem|matitem|miningtool|thrownitem|tillingtool|wiretool)$' | awk -F, '{printf "%s,", $1}'`
-	time python app.py --dest dest --keys 'description' --files "${files}"
+	python app.py --dest dest --keys 'description' --files "${files}"
 done
