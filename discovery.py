d2d2f1c44dd114e82a1f9f6e1394fa399ed6348e
rog-works <rog_works@yahoo.co.jp>
2020-06-12 09:30:00 +0900

    fix: add resource management

--- discovery.py
+++ discovery.py
@@ -21,9 +21,9 @@ class Discovery:
     def search_files(self, dir: str, pattern: str) -> List[str]:
         reg = re.compile(pattern)
         find = ['find', dir, '-type', 'f']
-        find_ret = subprocess.Popen(find, encoding='utf-8', stdout=subprocess.PIPE)
-        filepaths = find_ret.stdout.read().split('\n')
-        return [filepath for filepath in filepaths if reg.search(filepath)]
+        with subprocess.Popen(find, encoding='utf-8', stdout=subprocess.PIPE) as find_ret:
+            filepaths = find_ret.stdout.read().split('\n')
+            return [filepath for filepath in filepaths if reg.search(filepath)]
 
     def discover(self, data: dict) -> List[str]:
         return [elem.full_path for elem in JsonQuery(data).leaf() if elem.text.find(' ') != -1]
