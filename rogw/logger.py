0a5d70af386ab3899fac6c4375582479bc6da3d7
rog-works <rog_works@yahoo.co.jp>
2020-06-13 15:34:46 +0900

    refactor: improvement log format

--- rogw/logger.py
+++ rogw/logger.py
@@ -1,8 +1,10 @@
 import logging
 
 
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
+handler.setFormatter(formatter)
 logger.addHandler(handler)
