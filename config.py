57d8e04a9af6bb3e96f0d4bdf105f0bf765e2281
rog-works <rog_works@yahoo.co.jp>
2020-06-17 23:18:58 +0900

    fix: reset config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-config = {
-    'GAS_URL': 'TODO: gas web api url here.',
-    'REQUEST_SIZE_LIMIT': 5000,
-    'RECORD_FILEPATH': 'record.csv',
-    'DEST_DIR': 'dest/',
-}
---
+++ rogw/config.py
@@ -0,0 +1,8 @@
+config = {
+    'GAS_URL': 'https://example.com/path',
+    'REQUEST_SIZE_LIMIT': 4000,
+    'RECORD_FILEPATH': 'save/record.csv',
+    'DEST_DIR': 'dest/',
+    'CACHE_DIR': 'caches/',
+    'REPORT_FILEPATH': 'logs/report.log',
+}
